95b1caaeade7d0ed2e29ff81e17e33e5a55efe47
shanstarvish <142512864+shanstarvish@users.noreply.github.com>
2024-06-18 10:29:01 -0400

    Add files via upload

--- ProjectGame/7-3_Starvish.py
+++ ProjectGame/7-3_Starvish.py
@@ -6,7 +6,7 @@ rooms = {'Coop': {'name': 'Coop',
                   'West': 'Playhouse',
                   'East': 'Greenhouse',
                   'North': 'Garden',
-                  'item': 'No chicken here'},
+                  },
          'Barn': {'name': 'Barn',
                   'South': 'Greenhouse',
                   'item': 'Scout Wingtip'},
@@ -26,45 +26,16 @@ rooms = {'Coop': {'name': 'Coop',
                       'item': 'Villain Reynard the Fox'},
          'The Shed': {'name': 'The Shed',
                       'North': 'Coop',
-                      'East': 'Under the Tractor',
-                      'item': 'No Chicken here'},
+                      'East': 'Under the Tractor'},
          'Greenhouse': {'name': 'Greenhouse',
                         'North': 'Barn',
                         'West': 'Coop',
-                        'item': 'Boom-Boom Beak'}
+                        'item': 'Boom-Boom Beak'},
+         'Under the Tractor': {'name': 'Under the Tractor',
+                               'West': 'The Shed',
+                               'item': 'Agent Fluff'}
 
          }
-
-
-def main(user_input):
-    global current_room
-    while True:
-        print('You are currently in: {}'.format(current_room['name']))
-        print('-----------------------------------------')
-        print('Enter Exit at anytime to end the game')
-        # Add if statement for winning game here,
-        # Check for all the items list
-        user_input = input(
-            'You must start gathering your army now, what direction would you like to start searching?').capitalize()
-        print('-----------------------------------------')
-        if current_room['name'] == 'Exit':
-            print('You have decided to give up on this battle. Failing to avenge your fallen comrades,'
-                  'and disgracing all chicken kind.')
-
-        # Get user input for moving to each location
-
-        elif user_input in directions:
-            if user_input in current_room:
-                current_room = rooms[current_room[user_input]]
-                # Move is not possible, bad move
-            else:
-                print('There appears to be no chickens in that direction. Go another way')
-        elif user_input == 'Exit':
-            print('You have decided to give up on this battle. Failing to avenge your fallen comrades,'
-                  'and disgracing all chicken kind.')
-            current_room['name'] = 'Exit'
-            break
-    return user_input
 
 
 def instructions():
@@ -79,15 +50,63 @@ def instructions():
     print('Move Commands: North, South, East or West')
     print('You are starting your search in {}, the last known where abouts of the chickens before they '
           'scattered to the winds.'.format(current_room['name']))
-    user_input = input('What direction would you like go?')
+    return
+
+
+def main(user_input):
+    global current_room
+    while True:
+        print('-----------------------------------------')
+        print('Enter Exit at anytime to end the game')
+        # Add if statement for winning game here,
+        # Check for all the items list
+        user_input = input(
+            'You must start gathering your army now, what direction would you like to start searching? ').capitalize()
+        print('-----------------------------------------')
+        if current_room['name'] == 'Exit':
+            print('You have decided to give up on this battle. Failing to avenge your fallen comrades,'
+                  'and disgracing all chicken kind.')
+
+        # Get user input for moving to each location
+
+        elif user_input in directions:
+            if user_input in current_room:
+                current_room = rooms[current_room[user_input]]
+                print('You are currently in: {}'.format(current_room['name']))
+                get_item(current_room)
+
+            else:
+                # Move is not possible, bad move
+                print('There appears to be no chickens in that direction. Go another way')
+        elif user_input == 'Exit':
+            print('You have decided to give up on this battle. Failing to avenge your fallen comrades,'
+                  'and disgracing all chicken kind.')
+            current_room['name'] = 'Exit'
+            break
     return user_input
 
+
+def get_item(item):
+    # figure out how to get user_input for collecting chicken
+    # add to list
+    global current_room
+    item = current_room
+
+    if 'item' in item:
+        print(item['item'])
+        #print('You have found {}!'.format(chick))
+        #chick.append(chicken_army)
+        print(chicken_army)
+        #return chicken_army
+    else:
+        print('No item here')
+        return item
 
 
 # Beginning of Code
 directions = ['North', 'South', 'East', 'West']  # list of available commands / directions
 current_room = rooms['Coop']  # Starting location for General Talon / player
+chicken_army = []
 
 instructions()
 main(current_room)
-
